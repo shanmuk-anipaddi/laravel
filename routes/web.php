0eb5a28d5e183bb81cc0452777239a9424ebadeb
shanmuk <shanmukha.anipaddi@adsiduous.com>
2022-09-19 14:27:15 -0700

    laravel route changes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostControllerResource;
-use App\Models\Post;
-
+use App\Models\post;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +19,6 @@ use App\Models\Post;
 //     return view('welcome');
 // });
 
-
 // Route::get('/about',function(){
 //     //return view('About us ')
 //     return "Hi this about!";
@@ -35,31 +33,63 @@ use App\Models\Post;
 //     return "this url is ".$url;
 // }));
 
-Route::get('/post/{id}/{name}/{pwd}',[PostControllerResource::class, 'post']);
+Route::get('/post/{id}/{name}/{pwd}', [PostControllerResource::class, 'post']);
 
-Route::get('/contact',[PostControllerResource::class,'contact']);
+Route::get('/contact', [PostControllerResource::class, 'contact']);
 
 //Route::get('/post/{id}',[PostControllerResource::class, 'show']);
 //Route::get('/post','PostControllerResource@index');
 
-Route::get('/Read',function(){
+Route::get('/Read', function () {
     $posts = Post::all();
-    foreach($posts as $post){
-        //echo "<pre>";print_r($posts);echo "</pre>";        
-        echo  $post->title."<br>";
+    foreach ($posts as $post) {
+        //echo "<pre>";print_r($posts);echo "</pre>";
+        echo $post->title . '<br>';
     }
 });
 
-Route::get('/find',function(){
+Route::get('/find', function () {
     $posts = Post::find(2);
     //foreach($posts as $post){
-        //echo "<pre>";print_r($posts);echo "</pre>";        
-        echo  $posts->title."<br>";
+    //echo "<pre>";print_r($posts);echo "</pre>";
+    echo $posts->title . '<br>';
     //}
 });
 
-Route::get('/findmore',function(){
-    $posts = Post::findOrFail(2);
+Route::get('/findmore', function () {
+    $posts = Post::findOrFail(1);
 
     return $posts;
+});
+
+/*
+    ELOQUENT INSERTING DATA
+*/
+
+Route::get('/insert', function () {
+    $post = new Post();
+
+    $post->title = 'This is test title 3';
+    $post->body = 'This is test body from insert function 3';
+
+    $post->save();
+});
+
+/*
+    ELOQUENT UPDATING DATA
+*/
+Route::get('/saveupdate/{id}', function ($id) {
+    $post = Post::find($id);
+
+    $post->title = 'This is updated title from save';
+
+    $post->save();
+});
+
+/*
+    ELOQUENT DELETE DATA
+*/
+Route::get('/delete/{id}', function ($id) {
+    $post = Post::find($id);
+    $post->delete();
 });
